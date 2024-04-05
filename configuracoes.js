78482eb0eabb8f0968fd8191554e37691d3bb7f3
Adriano Victor N. Ribeiro <115894997+adrianovictorn@users.noreply.github.com>
2024-04-05 17:39:32 -0300

    Update configuracoes.js

--- configuracoes.js
+++ configuracoes.js
@@ -1,19 +1,23 @@
-const urlUF = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
-const cidade = document.getElementById('city')
-const uf = document.getElementById ("uf");
+const urlUF = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+const cidade = document.getElementById('city');
+const ufSelect = document.getElementById('uf');
+const cityList = document.getElementById('cityList');
 
-uf.addEventListener('change', async function(){
-    const urlCidade = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/' +uf.value+ '/municipios'
-    const request = await fetch(urlCidade)
-    const response = await request.json()
+ufSelect.addEventListener('change', async function(){
+    const uf = this.value;
+    const urlCidade = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`;
+    const request = await fetch(urlCidade);
+    const response = await request.json();
     
-    let options = ''
-    response.forEach(function(cidades){
-        options += '<option>'+cidades.nome+'</option>'
-    })
+    cityList.innerHTML = ''; 
 
-    city.innerHTML = options
-})
+    response.forEach(function(cidade){
+        const option = document.createElement('option');
+        option.value = cidade.nome;
+        cityList.appendChild(option);
+    });
+});
+
 
 window.addEventListener('load', async ()=>{
     const request = await fetch(urlUF);
